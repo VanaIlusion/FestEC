5da4f099a4e3c2e862df697c0127b401a27fe96b
theWind <1542602084@qq.com>
2017-08-11 02:46:32 +0800

    完善RestClient

--- latte-core/src/main/java/com/wind/latte/net/RestClient.java
+++ latte-core/src/main/java/com/wind/latte/net/RestClient.java
@@ -10,10 +10,13 @@ import com.wind.latte.net.callback.RequestsCallbacks;
 import com.wind.latte.ui.LatteLoader;
 import com.wind.latte.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-import okhttp3.ResponseBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 
@@ -24,12 +27,13 @@ import retrofit2.Callback;
 public class RestClient {
 
     private final String URL;
-    private static final WeakHashMap<String,Object> PARAMS = RestCreator.getParams();
+    private static final WeakHashMap<String, Object> PARAMS = RestCreator.getParams();
     private final IRequest REQUEST;
     private final ISuccess SUCCESS;
     private final IFailure FAILURE;
     private final IError ERROR;
-    private final ResponseBody BODY;
+    private final RequestBody BODY;
+    private final File FILE;
     private LoaderStyle LOADER_STYLE;
     private Context CONTEXT;
 
@@ -39,7 +43,8 @@ public class RestClient {
                       ISuccess success,
                       IFailure failure,
                       IError error,
-                      ResponseBody body,
+                      RequestBody body,
+                      File file,
                       Context context,
                       LoaderStyle loaderStyle) {
         this.URL = url;
@@ -49,64 +54,90 @@ public class RestClient {
         this.FAILURE = failure;
         this.ERROR = error;
         this.BODY = body;
+        this.FILE = file;
         this.CONTEXT = context;
         this.LOADER_STYLE = loaderStyle;
     }
 
-    public static RestClientBuilder builder(){
+    public static RestClientBuilder builder() {
         return new RestClientBuilder();
     }
 
-    private void request(HttpMethod method){
+    private void request(HttpMethod method) {
         final RestService service = RestCreator.getRestService();
         Call<String> call = null;
         //开始请求
-        if (REQUEST!=null){
+        if (REQUEST != null) {
             REQUEST.onRequestStart();
         }
         //显示loading
-        if (LOADER_STYLE!=null){
-            LatteLoader.showLoading(CONTEXT,LOADER_STYLE);
+        if (LOADER_STYLE != null) {
+            LatteLoader.showLoading(CONTEXT, LOADER_STYLE);
         }
-        switch (method){
+        switch (method) {
             case GET:
-                call = service.get(URL,PARAMS);
+                call = service.get(URL, PARAMS);
                 break;
             case POST:
-                call = service.post(URL,PARAMS);
+                call = service.post(URL, PARAMS);
+                break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
                 break;
             case PUT:
-                call = service.put(URL,PARAMS);
+                call = service.put(URL, PARAMS);
+                break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
                 break;
             case DELETE:
-                call = service.delete(URL,PARAMS);
+                call = service.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody = RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body = MultipartBody.Part.createFormData("file", FILE.getName(), requestBody);
+                call = RestCreator.getRestService().upload(URL, body);
                 break;
             default:
                 break;
         }
 
-        if (call!=null){
+        if (call != null) {
             call.enqueue(getRequestCallback());
         }
     }
 
-    private Callback<String> getRequestCallback(){
-        return new RequestsCallbacks(REQUEST,SUCCESS,FAILURE,ERROR,LOADER_STYLE);
+    private Callback<String> getRequestCallback() {
+        return new RequestsCallbacks(REQUEST, SUCCESS, FAILURE, ERROR, LOADER_STYLE);
     }
 
-    public final void get(){
+    public final void get() {
         request(HttpMethod.GET);
     }
 
-    public final void post(){
-        request(HttpMethod.POST);
+    public final void post() {
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null!");
+            }
+            request(HttpMethod.POST_RAW);
+        }
     }
 
-    public final void put(){
-        request(HttpMethod.PUT);
+    public final void put() {
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null!");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
-    public final void delete(){
+    public final void delete() {
         request(HttpMethod.DELETE);
     }
 }
--- latte-core/src/main/java/com/wind/latte/net/RestClientBuilder.java
+++ latte-core/src/main/java/com/wind/latte/net/RestClientBuilder.java
@@ -8,11 +8,12 @@ import com.wind.latte.net.callback.IRequest;
 import com.wind.latte.net.callback.ISuccess;
 import com.wind.latte.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 import okhttp3.MediaType;
-import okhttp3.ResponseBody;
+import okhttp3.RequestBody;
 
 /**
  * Created by theWind on 2017/8/9.
@@ -23,13 +24,14 @@ public class RestClientBuilder {
 
     private String mUrl = null;
     private static final Map<String, Object> PARAMS = RestCreator.getParams();
-    private IRequest mIRequest= null;
-    private ISuccess mISuccess= null;
-    private IFailure mIFailure= null;
-    private IError mIError= null;
-    private ResponseBody mBody= null;
-    private Context mContext= null;
-    private LoaderStyle mLoaderStyle= null;
+    private IRequest mIRequest = null;
+    private ISuccess mISuccess = null;
+    private IFailure mIFailure = null;
+    private IError mIError = null;
+    private RequestBody mBody = null;
+    private File mFile = null;
+    private Context mContext = null;
+    private LoaderStyle mLoaderStyle = null;
 
     //不允许外部包的类new它的实例，本包内可以，不同于private
     RestClientBuilder() {
@@ -46,13 +48,22 @@ public class RestClientBuilder {
     }
 
     public final RestClientBuilder params(String key, Object value) {
-
         this.PARAMS.put(key, value);
         return this;
     }
 
+    public final RestClientBuilder file(File file) {
+        this.mFile = file;
+        return this;
+    }
+
+    public final RestClientBuilder file(String file) {
+        this.mFile = new File(file);
+        return this;
+    }
+
     public final RestClientBuilder raw(String raw) {
-        this.mBody = ResponseBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
+        this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
         return this;
     }
 
@@ -77,17 +88,19 @@ public class RestClientBuilder {
         return this;
     }
 
-    public final RestClientBuilder loader(Context context,LoaderStyle loaderStyle){
+    public final RestClientBuilder loader(Context context, LoaderStyle loaderStyle) {
         this.mContext = context;
         this.mLoaderStyle = loaderStyle;
         return this;
     }
-    public final RestClientBuilder loader(Context context){
+
+    public final RestClientBuilder loader(Context context) {
         this.mContext = context;
         this.mLoaderStyle = LoaderStyle.BallClipRotatePulseIndicator;//默认的loading样式;
         return this;
     }
-    public final  RestClient build(){
-        return new RestClient(mUrl,PARAMS,mIRequest,mISuccess,mIFailure,mIError,mBody,mContext,mLoaderStyle);
+
+    public final RestClient build() {
+        return new RestClient(mUrl, PARAMS, mIRequest, mISuccess, mIFailure, mIError, mBody, mFile, mContext, mLoaderStyle);
     }
 }
